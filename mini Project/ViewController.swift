b3bc8a019692325e5a600d6c994a06bff8ab3005
Usman Kulaha <32143087+UK818@users.noreply.github.com>
2022-06-07 08:46:05 +0100

    Initial Commit

---
+++ mini Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mini Project
+//
+//  Created by mac on 07/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
